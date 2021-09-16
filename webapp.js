78244f4cbdfb39fa0bd708447930f20a748448b5
Francisco Oliveira <francisco.m.gomes.oliveira@gmail.com>
2021-09-16 17:33:32 +0100

    Create webapp.js

---
+++ webapp.js
@@ -0,0 +1,33 @@
+<script type="module">
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyDD-F2GXcH2nNOK_RIEyE3J0LzbUN7VmL8",
+
+    authDomain: "franciscooliveira-website.firebaseapp.com",
+
+    projectId: "franciscooliveira-website",
+
+    storageBucket: "franciscooliveira-website.appspot.com",
+
+    messagingSenderId: "820611016665",
+
+    appId: "1:820611016665:web:6741ae4e7e424f0386dbc8"
+
+  };
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+</script>
